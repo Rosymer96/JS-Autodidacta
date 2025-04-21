81c6d1f6a6be2fe256a664d9611e99dfd07da243
Rosa Vela <rosymer96@gmail.com>
2025-04-21 11:07:39 +0200

    Estudio con curso JS Udemy

---
+++ JavaScript/main.js
@@ -0,0 +1,12 @@
+// "ho-ho-ho".split("-"); // ['ho', 'ho', 'ho']
+
+// console.log("ho-ho-ho".split("-"))
+
+// console.log("Rosa Mercedes Vela".split(" "));
+
+let a = ['fernando', 'rosa', 'Alessia'];
+
+// a.pop;
+
+console.log(a.pop());
+console.log(a);
